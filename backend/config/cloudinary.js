7eeef5e6e08a793df23bcf8f2cb80332262ab69e
Ishikaraj-30 <ishikaraj3030@gmail.com>
2026-04-26 18:47:15 +0530

    still error fixing

--- backend/config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: "dkzufz0zk",
-  api_key: "995266142295157",
-  api_secret: "SBkX5iaTaUVI9c6MlHAH_TgZ4ps"
-});
-
-module.exports = cloudinary;
